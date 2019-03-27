4c660e99e4b6b02810d1fa8bb239483be4104a2e
olegagapov <olegagapov.ya@yandex.ru>
2019-03-27 17:55:04 +0300

    1_SiteController

---
+++ app/Http/Controllers/SiteController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SiteController extends Controller
+{
+    public function main($id = '', $i = 0)
+    {
+         $ii = ''; if($i !== 0) $ii = $i;
+        return view('main', [
+            'id' => $id,
+            'i' => $i,
+            'ii' => $ii
+        ]);
+    }
+
+    public function pages($head, $content = 'default', $footer = 'default'){
+        return view('main', [
+            'head' => $head.'  head pages',
+            'content' => $content.'  content pages',
+            'footer' => $footer.' footer pages'
+        ]);
+    }
+}
