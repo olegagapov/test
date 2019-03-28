1b888e4f3fa75c32257db4cfd2ed73fc28bb01bd
olegagapov <olegagapov.ya@yandex.ru>
2019-03-28 23:49:29 +0300

    tanusha

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Таня я тебя люблю
+                    Танюша я тебя люблю! Твой Олег.
                 </div>
 
 
